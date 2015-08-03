bca8c11f625bdc9b989cad9f91c0424346d06493
seglo <seglo@randonom.com>
2015-08-03 13:40:10 -0400

    Use BigDecimal instead of double for prices.  Simplify Order.total impl.

--- src/main/scala/BundleProcessor.scala
+++ src/main/scala/BundleProcessor.scala
@@ -4,27 +4,27 @@ import org.joda.money.{CurrencyUnit, Money}
 
 import scala.annotation.tailrec
 
-trait OrderEntry
+trait OrderEntry {
+  def name: String
+  def price: BigDecimal
+}
 
-case class CatalogItem(name: String, price: Double) extends OrderEntry
-case class Bundle(name: String, items: Seq[CatalogItem], price: Double) extends OrderEntry
+case class CatalogItem(name: String, price: BigDecimal) extends OrderEntry
+case class Bundle(name: String, items: Seq[CatalogItem], price: BigDecimal) extends OrderEntry
 
 case class Order(entries: Seq[OrderEntry]) {
   val cad = CurrencyUnit.of("CAD")
 
-  lazy val price =
-    entries.foldLeft(Money.of(cad, 0d)) { (total: Money, orderEntry) =>
-      orderEntry match {
-        case CatalogItem(name, price) => total.plus(Money.of(cad, price))
-        case Bundle(name, items, price) => total.plus(Money.of(cad, price))
-      }
+  lazy val total: BigDecimal =
+    entries.foldLeft(Money.of(cad, BigDecimal(0).bigDecimal)) {
+      (total: Money, orderEntry) => total.plus(Money.of(cad, orderEntry.price.bigDecimal))
     }.getAmount
 }
 
 object BundleProcessor {
   def calculate(order: Order, allBundles: Set[Bundle]): Order = {
     val orders = orderPermutations(order, allBundles)
-    orders.minBy(_.price)
+    orders.minBy(_.total)
   }
 
 
@@ -46,13 +46,13 @@ object BundleProcessor {
         orderAcc
     }
 
-    go(Set(originalOrder), allBundles, Set.empty[Order])
+    go(Set(originalOrder), allBundles, Set(originalOrder))
   }
 
   def applyBundleToOrder(order: Order, bundle: Bundle): Option[Order] = {
     val bundledEntries = order.entries.diff(bundle.items)
-    // if all bundle items have been removed from the order then the bundle
-    // was successfully applied.  return the updated order with the bundle
+    // If all bundle items have been removed from the order by using diff then the
+    // bundle was successfully applied.  Return the updated order with the bundle.
     if (bundledEntries.length == order.entries.length - bundle.items.length)
       Some(Order(bundledEntries :+ bundle))
     else
